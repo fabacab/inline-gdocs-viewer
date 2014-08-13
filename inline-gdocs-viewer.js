74923639f3750e1322096e7a8fe7892be9db5f4b
meitar <meitar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-13 20:29:25 +0000

    Scroll DataTables-enhanced tables horizontally by default.
    git-svn-id: http://svn.wp-plugins.org/inline-google-spreadsheet-viewer/trunk@965312 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inline-gdocs-viewer.js
+++ inline-gdocs-viewer.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function () {
     jQuery('.igsv-table:not(.no-datatables)').each(function () {
         jQuery(this).dataTable({
             'dom': 'TC<"clear">lfrtip',
+            'scrollX': true,
             'tableTools': {
                 'sSwfPath': '//datatables.net/release-datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf'
             }
